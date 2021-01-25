556b914272a84b316b88bec98274de750a337154
charleechan <charleechan@163.com>
2021-01-25 09:09:09 +0800

    fix bugs

--- md2docs.bat
+++ md2docs.bat
@@ -1,3 +1,4 @@
-@echo
+copy .\mds\SoftTuto\Manage\Git\res\mermaid.min.js .\mds\node_modules\mermaid\dist
+copy .\mds\SoftTuto\Manage\Git\res\mermaid.min.js .\mds\node_modules\gitbook-plugin-mermaid-gb3\dist\mermaid
 gitbook build ./mds/. ./docs
 pause
